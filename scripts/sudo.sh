c3d5c0e5635db52c8906237238d75a4487eacfb9
Don Ky <don.d.ky@gmail.com>
2014-06-10 23:33:17 -0400

    Modified sudoer script to support both squeeze + wheezy

--- scripts/sudo.sh
+++ scripts/sudo.sh
@@ -4,9 +4,9 @@ SUDO_FILE=/etc/sudoers
 
 if egrep -i "vagrant" $SUDO_FILE
 then
-   echo "SUDO entries Already exist"
+	echo "SUDO entries Already exist"
 else
 	echo "Configuring SUDO for vagrant"
-	sed -i "/root\s*ALL=(ALL:ALL) ALL/ a\vagrant ALL=NOPASSWD:ALL" /etc/sudoers
-   echo "Defaults env_keep=“SSH_AUTH_SOCK" >> /etc/sudoers
+	sed -i "/root\s*ALL=*/ a\vagrant ALL=NOPASSWD:ALL" /etc/sudoers
+	echo "Defaults env_keep=“SSH_AUTH_SOCK" >> /etc/sudoers
 fi
